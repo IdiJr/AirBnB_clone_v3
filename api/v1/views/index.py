0daa37ac4530dd6cd92f09aa065e8a676911bd8c
root <root@DESKTOP-KUT53MR>
2023-10-02 16:39:05 +0000

    intet

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,28 +3,23 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """Returns API Json satus"""
+    """returns a JSON string of the status in a 200 response"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def getstats():
+def get_stats():
     """retrieves the number of each objects by type"""
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objt = {}
-    for i in range(len(classes)):
-        num_objt[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objt)
+    objts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        }
+    return jsonify(objts)
