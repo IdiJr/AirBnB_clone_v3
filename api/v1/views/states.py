904d14740099be6b9e801185d9ed927f2f2778e5
IdiJr <idialijnr@gmail.com>
2023-10-02 13:15:55 +0000

    State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -51,7 +51,7 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """Updates a State object by ID"""
+    """Updates a state object by ID"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
