64b476aa88572579758032dd957cf02d00d35559
root <root@DESKTOP-KUT53MR>
2023-10-02 17:13:44 +0000

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 """index.py module page"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """returns a JSON string of the status in a 200 response"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'])
 def get_stats():
     """retrieves the number of each objects by type"""
-    objts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
         }
-    return jsonify(objts)
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
